298970d9b8646f3f1b8daf60a715429eb651faf1
David Petkov <davidpetkov2006@gmail.com>
2023-10-25 19:09:40 +0300

    Created map

--- src/SoftwareFest/ViewModels/RegisterViewModel.cs
+++ src/SoftwareFest/ViewModels/RegisterViewModel.cs
@@ -1,8 +1,20 @@
-﻿namespace SoftwareFest.ViewModels
+﻿using AutoMapper;
+using SoftwareFest.Infrastructure.Mapping;
+using SoftwareFest.Models;
+
+namespace SoftwareFest.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : IMapFrom<ApplicationUser>
     {
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
+
+        public void Mapping(Profile mapping)
+        {
+            mapping.CreateMap<RegisterViewModel, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore()).ReverseMap();
+        }
     }
 }
