3cd9062a97bbe98fd0cb9b2136f04b8d4b950c70
David Hristov <orionvt3@gmail.com>
2023-10-27 12:56:58 +0300

    stripe base logic

---
+++ src/SoftwareFest/Controllers/StripeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareFest.Services.Contracts;
+using Stripe;
+
+namespace SoftwareFest.Controllers
+{
+    [Route("[controller]")]
+    public class StripeController : Controller
+    {
+        private readonly IConfiguration _config;
+        public StripeController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+
+        [HttpGet("connect")]
+        public async Task<IActionResult> ConnectStripe()
+        {
+            var cliendId = _config["Stripe:ClientId"];
+            var redirectUrl = "https://localhost:7215/stripe/callback";
+            return Redirect(
+                $"https://connect.stripe.com/oauth/authorize?response_type=code&client_id={cliendId}&scope=read_write&redirect_uri={redirectUrl}");
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> StripeCallback(string code)
+        {
+            var options = new OAuthTokenCreateOptions
+            {
+                Code = code,
+                GrantType = "authorization_code"
+            };
+
+            var service = new OAuthTokenService();
+            var response = await service.CreateAsync(options);
+            
+            //TODO:Save AC to DB
+
+            return Redirect("/somePage");
+        }
+    }
+}
---
+++ src/SoftwareFest/Services/CheckoutService.cs
@@ -0,0 +1,56 @@
+﻿using SoftwareFest.Services.Contracts;
+using SoftwareFest.ViewModels;
+using Stripe.Checkout;
+
+namespace SoftwareFest.Services
+{
+    public class CheckoutService : ICheckoutService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CheckoutService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<string> CheckOut(ProductViewModel product)
+        {
+            //var baseUrl = _httpContextAccessor.HttpContext.Request.Host.ToString();
+            var baseUrl = "https://localhost:7215";
+
+            var options = new SessionCreateOptions
+            {
+                // Stripe calls the URLs below when certain checkout events happen such as success and failure.
+                SuccessUrl = $"{baseUrl}/checkout/success?sessionId=" + "{CHECKOUT_SESSION_ID}", // Customer paid.
+                CancelUrl = baseUrl + "/failed",  // Checkout cancelled.
+                PaymentMethodTypes = new List<string> // Only card available in test mode?
+                {
+                    "card"
+                },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new()
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = product.Price, // Price is in USD cents.
+                            Currency = "USD",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = product.Name,
+                                Description = product.Description,
+                                Images = new List<string> { product.ImageUrl }
+                            },
+                        },
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment" // One-time payment. Stripe supports recurring 'subscription' payments.
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+
+            return session.Id; ;
+        }
+    }
+}
